2c3760cc853f12455e8155911090f5337f2f5ff7
Lucky <andresssmi@outlook.com>
2025-11-14 00:56:21 -0600

    Corrigiendo la conexión a la DB y añadiendo SSL para Render

--- backend/database.js
+++ backend/database.js
@@ -1,12 +1,15 @@
-const { Pool } = require ( 'pg' )
+const { Pool } = require('pg');
+// No es necesario require('dotenv') si ya lo tienes en index.js, pero lo dejamos por si acaso.
+require('dotenv').config(); 
 
+// Render usa la variable DATABASE_URL que tú configuraste
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'consultorio_db',
-    password: 'mimientzi1312',
-
-    port: 5432,
+    connectionString: process.env.DATABASE_URL,
+    
+    // Esta configuración es OBLIGATORIA para las conexiones seguras de Render.
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = pool;
